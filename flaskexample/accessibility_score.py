8534cf5026c6c3e739e1ed71ddee9099af91767c
natalie korn <natalie.j.korn@gmail.com>
2019-10-02 18:16:55 -0700

    changes to scoring

--- flaskexample/accessibility_score.py
+++ flaskexample/accessibility_score.py
@@ -2,6 +2,32 @@ import numpy as np
 import geopandas as gdp
 
 def accessibility_score(gdf):
-    score = np.nanmin(gdf['PCI_Score'])
+    '''
+    Current overall accessibility metric
+    *future* customize based on custom user importance
+
+    Curb: Percentage of construction not involving changes to the curb
+
+    Pavement: Minimum pavement quality
+        Changed from average based on walking the streets
+
+    Width: Percentage of sidewalks meeting estimated minimum width requriement
+    '''
+
+    print(gdf.columns)
+    # curb ramp work
+    if sum(gdf['curbrampwo'].astype(int)) == 0:
+        curb = 1
+    else:
+        curb = 1-(sum(gdf['curbrampwo'])/len(gdf))
+
+    # pavement quality
+    pavement = np.nanmin(gdf['PCI_Score'])/100
+
+    # sidewalk minimum width requirements
+    width = sum(gdf['route_width_preds'])/len(gdf)
+    #print("curb: {}, pavement: {}, width: {}".format(curb, pavement, width))
+
+    score=(curb+pavement+width)/3
 
     return score
